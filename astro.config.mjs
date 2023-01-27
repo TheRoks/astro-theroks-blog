25efa00c4d801a0c8c26542972371aab0752a8eb
Stefan Roks <stefan@theroks.com>
2023-01-27 20:26:49 +0100

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,120 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import image from '@astrojs/image';
+import mdx from '@astrojs/mdx';
+import partytown from '@astrojs/partytown';
+import compress from 'astro-compress';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeExternalLinks from 'rehype-external-links';
+
+import { SITE } from './src/config.mjs';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const whenExternalScripts = (items = []) =>
+  SITE.googleAnalyticsId ? (Array.isArray(items) ? items.map((item) => item()) : [items()]) : [];
+
+export default defineConfig({
+  site: SITE.origin,
+  base: SITE.basePathname,
+  trailingSlash: SITE.trailingSlash ? 'always' : 'never',
+
+  output: 'static',
+
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    sitemap(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    mdx(),
+
+    ...whenExternalScripts(() =>
+      partytown({
+        config: { forward: ['dataLayer.push'] },
+      })
+    ),
+
+    compress({
+      css: true,
+      html: {
+        removeAttributeQuotes: false,
+      },
+      img: false,
+      js: true,
+      svg: false,
+
+      logger: 1,
+    }),
+  ],
+
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'github-dark-dimmed',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: false,
+    },
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: 'append',
+          content: {
+            type: 'element',
+            tagName: 'span',
+            properties: { className: ['heading-link', 'not-prose'] },
+            children: [
+              {
+                type: 'element',
+                tagName: 'img',
+                properties: { src: '/assets/link.svg' },
+                children: [],
+              },
+            ],
+          },
+        },
+      ],
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+          content: {
+            type: 'element',
+            tagName: 'img',
+            properties: {
+              src: '/assets/external-link.svg',
+              alt: 'External link icon',
+            },
+            children: [],
+          },
+          contentProperties: { className: ['external-link-icon', 'not-prose'] },
+        },
+      ],
+    ],
+  },
+
+  vite: {
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
+      },
+    },
+  },
+});
---
+++ src/content/post/2013/angularjs-filter-number-fixed-length.md
@@ -0,0 +1,21 @@
+---
+title: "AngularJS Filter Number Fixed Length"
+path: "/angularjs-filter-number-fixed-length/"
+tags: ["AngularJS"]
+excerpt: "AngularJS Filter number fixed length. This filter show how you can create a filter to have leading 0 till a certain length has reached."
+publishDate: 2013-12-11
+updated: 2013-12-11
+---
+
+
+Sometimes you want a number of a certain length. If it’s not of that length it needs leading zeros. AngularJS provides the [number filter](https://docs.angularjs.org/api/ng/filter/number). However this lacks the possibility to have leading zeros.
+
+AngularJS is very extendable. You can make your own leading zeros filter in just a few minutes. The creation of a custom AngularJS filter needs three things.
+
+- Create a module
+- Create the filter and his function
+- Register the module in the application
+
+## The AngularJS Filter
+
+The JSFiddle belows shows a filter that gives you leading zeros.
