908038c54fc49318e4c400f0905044f90dd424f7
Stefan Roks <stefan@theroks.com>
2025-08-14 17:16:22 +0200

    chore: update icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { fileURLToPath } from "url";
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import mdx from "@astrojs/mdx";
@@ -28,6 +29,24 @@ export default defineConfig({
   output: "static",
 
   integrations: [
+    icon({
+      include: {
+        tabler: ["*"],
+        logos: ["*"]
+      },
+      svgoOptions: {
+        plugins: [
+          {
+            name: "preset-default",
+            params: {
+              overrides: {
+                removeDoctype: false,
+              },
+            },
+          },
+        ],
+      },
+    }),
     robotsTxt(),
     sitemap({
       serialize(item) {
